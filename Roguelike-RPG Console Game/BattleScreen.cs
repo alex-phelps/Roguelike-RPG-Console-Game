7e6b81ce95840183989fd0456997ec02f02f1487
amp200123 <amp200123@gmail.com>
2015-11-16 14:13:08 -0500

    Re-added some things

---
+++ Roguelike-RPG Console Game/BattleScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike_RPG_Console_Game
+{
+    public class BattleScreen
+    {
+        Random random;
+        private Player player;
+        private Enemy enemy;
+
+        public BattleScreen(Player player, Enemy enemy)
+        {
+            random = new Random();
+
+            this.player = player;
+            this.enemy = enemy;
+        }
+
+        public void DoBattle()
+        {
+            bool? battleWon = null;
+
+            while (battleWon != null)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Roguelike-RPG Console Game/Enemy.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike_RPG_Console_Game
+{
+    public enum EnemyType
+    {
+        rat
+    }
+
+    public class Enemy
+    {
+        public int x { get; protected set; }
+        public int y { get; protected set; }
+
+        protected int level;
+        protected int baseHealth;
+        protected int health;
+        protected int baseAttack;
+        protected int attackDamage;
+        protected int baseDefence;
+        protected int defence;
+        protected int expDropBase;
+        protected int expDropped;
+        protected int goldDropBase;
+        protected int goldDropped;
+
+        public void Update(Player player)
+        {
+            if (Math.Abs(player.x - x) > Math.Abs(player.y - y))
+            {
+                if (player.x > x)
+                {
+                    x++;
+                }
+                else if (player.x < x)
+                {
+                    x--;
+                }
+                else if (player.y < y)
+                {
+                    y--;
+                }
+                else if (player.y > y)
+                {
+                    y++;
+                }
+                
+            }
+            else
+            {
+                if (player.y < y)
+                {
+                    y--;
+                }
+                else if (player.y > y)
+                {
+                    y++;
+                }
+                else if (player.x > x)
+                {
+                    x++;
+                }
+                else if (player.x < x)
+                {
+                    x--;
+                }
+            }
+        }
+
+        public virtual char ToChar()
+        {
+            return '$';
+        }
+    }
+}
