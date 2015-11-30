e6db5c5ecb677b4f782887ee865da0a8df2f3c77
amp200123 <amp200123@gmail.com>
2015-11-30 11:56:58 -0500

    Balance Tweaks and new enemy + final status effects implementation. Needs balance testing again.

---
+++ Roguelike-RPG Console Game/Boneman.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike_RPG_Console_Game
+{
+    public class Boneman : Enemy
+    {
+        public Boneman(int x, int y) 
+            :base(x, y)
+        {
+            name = "Boneman";
+            random = new Random();
+            level = random.Next(7, 12);
+            int modifier = Convert.ToInt32(level * 1.5f);
+
+            baseHealth = 4;
+            baseAttack = 9;
+            baseDefence = 1;
+            expDropBase = 5;
+            goldDropBase = 2;
+            baseResist = 4;
+            baseMagic = 0;
+
+            maxHealth = baseHealth + modifier;
+            attackDamage = baseAttack + modifier;
+            defence = baseDefence + modifier;
+            expDropped = expDropBase + modifier;
+            goldDropped = goldDropBase + modifier;
+            resist = baseResist + modifier;
+            magic = baseMagic + modifier;
+
+            effect = WeaponEffect.penetrate;
+
+            health = maxHealth;
+        }
+
+        public override char ToChar()
+        {
+            return '¥';
+        }
+    }
+}
---
+++ Roguelike-RPG Console Game/DamagableEntity.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike_RPG_Console_Game
+{
+    public class DamagableEntity
+    {
+        public string healthBar
+        {
+            get
+            {
+                string healthBar = "[";
+                string statusString = "";
+
+                for (int i = 9; i > 0; i--)
+                {
+                    if ((float)health >= (float)(maxHealth * (i / 9f)))
+                        healthBar += "█";
+                    else healthBar += " ";
+                }
+
+                healthBar += "█]";
+
+                if (status == StatusEffect.burned)
+                    statusString = "\n(Burned)";
+                else if (status == StatusEffect.cursed)
+                    statusString = "\n(Cursed)";
+
+                return healthBar + statusString;
+            }
+        }
+
+        public int x;
+        public int y;
+        public bool alive { get; protected set; }
+
+        public int level;
+        public int maxHealth;
+        public int health;
+        public int attackDamage;
+        public int magic;
+        public int defence;
+        public int resist;
+
+        public StatusEffect status = StatusEffect.none;
+
+        protected string name;
+
+        public DamagableEntity()
+        {
+            alive = true;
+        }
+
+        public virtual void TakeDamage(int damage, WeaponEffect effect)
+        {
+            if (effect == WeaponEffect.burn)
+                status = StatusEffect.burned;
+            else if (effect == WeaponEffect.curse)
+                status = StatusEffect.cursed;
+
+            if (effect != WeaponEffect.penetrate)
+                damage -= defence / 2;
+
+            if (damage < 0)
+                damage = 0;
+
+            health -= damage;
+
+            if (status == StatusEffect.burned)
+                health -= 5;
+            else if (status == StatusEffect.cursed)
+                health = (int)Math.Ceiling((4f / 5f) * health);
+
+            if (health <= 0)
+                alive = false;
+        }
+
+        public virtual void TakeMagicDamage(int magicDamage, WeaponEffect effect)
+        {
+            if (effect == WeaponEffect.burn)
+                status = StatusEffect.burned;
+            else if (effect == WeaponEffect.curse)
+                status = StatusEffect.cursed;
+
+            magicDamage -= resist / 3;
+
+            if (magicDamage < 0)
+                magicDamage = 0;
+
+            health -= magicDamage;
+
+            if (status == StatusEffect.burned)
+                health -= 5;
+            else if (status == StatusEffect.cursed)
+                health = (int)Math.Ceiling((4f / 5f) * health);
+
+            if (health <= 0)
+                alive = false;
+        }
+
+        public virtual char ToChar()
+        {
+            return '$';
+        }
+    }
+}
